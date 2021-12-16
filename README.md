3bd5d1c6dc0c44a03d14813676c6ef2b8c1471f6
leon1555 <leon.chisholm@keyin.com>
2021-12-16 12:07:20 -0330

    add README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-#s4sprint1
+BSTSpringBoot
+
+This is the server side of a Java application that takes in an array of integers from user input and draws a balanced binary
+search tree. The server uses the Spring Boot REST API framework.
+User inputs from the client are stored in a MySQL database.
+
+This application was written by Leon Chisholm, Lyndon Loveys, and John O'Neill, borrowing code for reconstructing and
+printing tree diagrams from:
+M. Inden (2022) Binary Trees. In: Java Challenges. Apress, Berkeley, CA. https://doi.org/10.1007/978-1-4842-7395-1_9
+(available in the online O'Reilly library).
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import com.example.accessingdatarest.TreeInput;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-class DemoApplicationTests {
-
+class TreeInputTest {
     @Test
-    void contextLoads() {
+    void testConstructor() {
+        TreeInput actualTreeInput = new TreeInput();
+        actualTreeInput.setId(123);
+        actualTreeInput.setInput("[1, 2, 3]");
+        assertEquals(123, actualTreeInput.getId());
+        assertEquals("[1, 2, 3]", actualTreeInput.getInput());
     }
-
 }
